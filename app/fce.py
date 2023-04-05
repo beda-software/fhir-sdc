27851ccd485d86963f7ae24192943e1a6abe09e6
Ilya Beda <ir4y.ix@gmail.com>
2023-04-05 17:44:56 +0400

    Add drafts for fce

---
+++ app/fce.py
@@ -0,0 +1,12 @@
+def to_first_class_extension(q):
+    # TODO implement conversion
+    # from FHIR format to first class extesion format
+    # for Questionnaire and QuestionnaireResponse resources
+    return q
+
+
+def from_first_class_extension(q):
+    # TODO implement conversion
+    # from first class extesion format to FHIR
+    # for Questionnaire and QuestionnaireResponse resources
+    return q
