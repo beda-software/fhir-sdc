50904c8def22f2a70c0e2161a3630abc52d31b44
Dmitry Shutov <dmitry.s@beda.software>
2023-04-18 10:39:34 +0700

    Add arbitrary fhir app init

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,17 @@ class Settings(AidboxSettings):
     SDC_VERSION = Required(v_type=str)
     JUTE_SERVICE = Required(v_type=str)
 
+class FHIRAppSettings():
+    SDC_VERSION = Required(v_type=str)
+    JUTE_SERVICE = Required(v_type=str)
+    BASE_URL = Required(v_type=str)
 
 settings = Settings(
     SDC_VERSION=os.getenv("SDC_VERSION", "2.7.0"), JUTE_SERVICE=os.getenv("JUTE_SERVICE", "aidbox")
+)
+
+fhir_app_settings = FHIRAppSettings(
+    SDC_VERSION=os.getenv("SDC_VERSION", "2.7.0"),
+    JUTE_SERVICE=os.getenv("JUTE_SERVICE", "aidbox"),
+    BASE_URL=os.getenv("BASE_URL", "http://devbox:8080/fhir"),
 )
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import logging
+import os
 
 import coloredlogs
 import sentry_sdk
 from aidbox_python_sdk.main import init as init_aidbox_app
 from aidbox_python_sdk.main import setup_routes as setup_aidbox_app_routes
 from aiohttp import web
+from fhirpy.lib import AsyncFHIRClient
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 
 from app.aidbox import sdk
+from app.fhir_server import routes as fhir_routes
+from app.settings import fhir_app_settings
 
 coloredlogs.install(level="DEBUG", fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logging.basicConfig(
@@ -25,7 +29,14 @@ sentry_logging = LoggingIntegration(
 sentry_sdk.init(integrations=[AioHttpIntegration(), sentry_logging])
 
 
-def create_app():
+async def create_gunicorn_app():
+    if os.getenv("USE_AIDBOX", "True").lower() == "true":
+        return create_aidbox_app()
+    else:
+        return create_fhir_app()
+
+
+def create_aidbox_app():
     app = web.Application()
     app.cleanup_ctx.append(init_aidbox_app)
     app.update(
@@ -36,5 +47,14 @@ def create_app():
     return app
 
 
-async def create_gunicorn_app():
-    return create_app()
+async def fhir_app_on_startup(app: web.Application):
+    app["settings"] = fhir_app_settings
+    app["client"] = AsyncFHIRClient(fhir_app_settings.BASE_URL)
+
+
+def create_fhir_app():
+    app = web.Application()
+    app.add_routes(fhir_routes)
+    app.on_startup.append(fhir_app_on_startup)
+
+    return app
