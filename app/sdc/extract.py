3f6743d81115094c11e0194f38fd9ad8f1c7293a
qscgyjqscgyj <aliestarten@gmail.com>
2025-02-25 15:59:33 +0100

    Updated extract to compile mappers in one transaction bundle
    Updated extract tests
    
    Ref https://github.com/beda-software/fhir-sdc/issues/76

--- app/sdc/extract.py
+++ app/sdc/extract.py
@@ -1,3 +1,5 @@
+import itertools
+
 from aiohttp import ClientSession, web
 
 
@@ -17,20 +19,36 @@ async def external_service_extraction(client, service, template, context):
                 raise web.HTTPBadRequest(body=await result.text())
 
 
+async def execute_jute_mappers_bundles(client, mappers_bundles):
+    not_transaction = any(bundle.get("type") != "transaction" for bundle in mappers_bundles)
+
+    result_bundle = {
+        "resourceType": "Bundle",
+        "type": "batch" if not_transaction else "transaction",
+        "entry": list(itertools.chain.from_iterable(bundle["entry"] for bundle in mappers_bundles)),
+    }
+
+    return await client.execute("/", data=result_bundle)
+
+
 async def extract(client, mappings, context, extract_services):
     """
     mappings could be a list of Aidbox Mapping resources
     or plain jute templates
     """
     resp = []
+    jute_mappers_bundles = []
 
     for mapper in mappings:
         if "resourceType" in mapper and "body" in mapper:
             # It is custome mapper resource
             mapper_type = mapper.get("type", "JUTE")
             if mapper_type == "JUTE" and extract_services["JUTE"] == "aidbox":
-                # Aidbox native extraction
-                resp.append(await mapper.execute("$apply", data=context))
+                mapper_bundle = await mapper.execute("$debug", data=context)
+                if "entry" not in mapper_bundle:
+                    continue
+
+                jute_mappers_bundles.append(mapper_bundle)
             else:
                 # Use 3rd party service FHIRPathMapping or JUTE
                 resp.append(
@@ -43,4 +61,8 @@ async def extract(client, mappings, context, extract_services):
             resp.append(
                 await external_service_extraction(client, extract_services["JUTE"], mapper, context)
             )
+
+    if len(jute_mappers_bundles) > 0:
+        resp.append(await execute_jute_mappers_bundles(client, jute_mappers_bundles))
+
     return resp
