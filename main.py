efc0874dfcb63b4f2db20aa45f3aebd95aa9eb26
Dmitry Shutov <dmitry.s@beda.software>
2025-05-13 13:21:47 +0500

    Remove DB client initialization

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from fhirpy.lib import AsyncFHIRClient
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 
-coloredlogs.install(level="DEBUG", fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+coloredlogs.install(
+    level="DEBUG", fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
 )
@@ -31,10 +33,17 @@ async def create_gunicorn_app():
         return create_app()
 
 
+async def init_aidbox_app(app):
+    from aidbox_python_sdk.main import init_client, register_app
+
+    app["client"] = await init_client(app["settings"])
+    await register_app(app["sdk"], app["client"])
+    yield
+
+
 # TODO: Add config param for aidbox-python-sdk pytest plugin
 # to select create_app
 def create_app():
-    from aidbox_python_sdk.main import init as init_aidbox_app
     from aidbox_python_sdk.main import setup_routes as setup_aidbox_app_routes
 
     from app.aidbox import sdk
@@ -53,7 +62,9 @@ async def fhir_app_on_startup(app: web.Application):
     from app.fhir_server.settings import settings
 
     app["settings"] = settings
-    app["client"] = AsyncFHIRClient(settings.BASE_URL, authorization=f"Basic {settings.AUTH_TOKEN}")
+    app["client"] = AsyncFHIRClient(
+        settings.BASE_URL, authorization=f"Basic {settings.AUTH_TOKEN}"
+    )
 
 
 def create_fhir_app():
