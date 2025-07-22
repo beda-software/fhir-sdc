95bccba24a31f0ab566b1c76dac9c2fe3082378e
Ilya Beda <ir4y.ix@gmail.com>
2025-07-22 20:35:50 +1000

    Fix fhirpath args

--- app/cached_fhirpath.py
+++ app/cached_fhirpath.py
@@ -4,10 +4,10 @@ from fhirpathpy import compile
 
 
 @lru_cache(maxsize=2**14)
-def cached_compile(expression):
-    return compile(expression)
+def cached_compile(expression, model=None):
+    return compile(expression, model)
 
 
 def fhirpath(context, expression, env=None, model=None):
-    compiled_expression = cached_compile(expression)
-    return compiled_expression(context, env or {}, model)
+    compiled_expression = cached_compile(expression, model)
+    return compiled_expression(context, env or {})
