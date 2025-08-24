b3c847fe6ed6cd14189d44d9ac9ed258adf5a225
Vadim Laletin <rusmiligamer@gmail.com>
2025-08-24 19:02:23 +0200

    feat: get rid of CREATE_MANIFEST_ATTRS

--- app/aidbox/sdk.py
+++ app/aidbox/sdk.py
@@ -2,6 +2,5 @@ from aidbox_python_sdk.sdk import SDK
 
 from .settings import settings
 
-from .manifest import manifest
 
-sdk = SDK(settings, resources=manifest)
+sdk = SDK(settings, resources={})
